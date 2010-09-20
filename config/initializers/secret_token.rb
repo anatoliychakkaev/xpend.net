f5ba6cdb955fa876c1f4614b15e3b0d12d45f409
Chakkaev Anatoliy <anatoliy.chakkaev@flatsoft.com>
2010-09-20 23:06:36 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XpendNet::Application.config.secret_token = '325775daf83ee74e879248574c17b75ae2d145c07043208323dc9b8f3485a73a0cc6f0d020e9783795b2158bd91020182b6d4da5992ce38546f8888524a402ec'
