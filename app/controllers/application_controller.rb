b528fe70da8e9b8e661c78ef778351a1e6e22193
Chakkaev Anatoliy <anatoliy.chakkaev@flatsoft.com>
2010-09-21 22:25:42 +0400

    Design landing layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    unless user_signed_in?
+      "landing"
+    else
+      if devise_controller?
+        "devise"
+      else
+        "application"
+      end
+    end
+  end
+
 end
